cc91fa9395878fc0c4850e7b3c66c753295626f7
samuelshix <samuel.li.shi@gmail.com>
2021-05-12 20:28:58 -0400

    first commit

---
+++ src/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,22 @@
+package com.craftinginterpreters.lox;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, MINUS, PLUS, COLON, SEMICOLON, SLASH, ASTR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL, OR,
+
+    // Literals.
+    IDENTIFIER, STRING, INT,
+
+    // Keywords.
+    AND, AMP, ELSE, FALSE, FUN, IF,
+    RETURN, TRUE, WHILE, BOOLTY, INTTY, FREE, MALLOC, GOTO,
+
+    EOF
+}
